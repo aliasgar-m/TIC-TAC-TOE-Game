10c95e837bdec786d71b8eff0c0432ce5fd3e447
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-11-01 18:15:13 +0530

    added hash value functions

--- src/agent.py
+++ src/agent.py
@@ -6,46 +6,36 @@ import random
 
 class Agent():
     def __init__(self, env: Environment, sym: int):
-        # super().__init__()
         self.env = env
         self.symbol = sym
         self.epsilon = 1.0
 
     def step(self):
         current_state = self.env.get_current_state()
-        # print(current_state)
         action = self.get_action(current_state)
         self.perform_action(current_state, action)
-        # current_state = new_state
-
+    
     def get_action(self, curr_state):
-        available_actions = self.get_available_actions(curr_state)
-        # print(available_actions)
+        available_actions = self.env.get_available_actions()
         search_prob = round(random.uniform(0,1),2)
         if search_prob <= self.epsilon:
             act = self.get_random_action(available_actions)
         else:
-            act = self.get_best_action(available_actions)
-        # print(act)
+            act = self.get_best_action(curr_state, available_actions)
         return act
-
-    def get_available_actions(self, state):
-        return np.argwhere(state == 0)
-
+    
     def get_random_action(self, actions):
         return random.choice(actions)
-    
-    def get_best_action(self, actions):
+
+    def get_best_action(self, state, actions):
+        state_hash = self.env.get_state_hash(state)
+        av_action_hash = self.env.get_action_hash(actions)
         return "Best choice selected"
 
     def perform_action(self, curr_state, action):
         x = action[0]
         y = action[1]
         curr_state[x][y] = curr_state[x][y] + self.symbol
-        print(action)
-        print(curr_state)
-        reward = 0
-        # return new_state, reward
 
     def reset_agent(self):
         pass
@@ -55,5 +45,12 @@ if __name__ == "__main__":
     env = Environment()
     p1 = Agent(env, sym=1)
     p2 = Agent(env, sym=2)
+    print(env.board)
     p1.step()
+    print(env.board)
     p2.step()
+    print(env.board)
+    p1.step()
+#     print(env.board)
+#     p2.step()
+#     print(env.board)
--- src/environment.py
+++ src/environment.py
@@ -9,14 +9,35 @@ class Environment:
         self.columns = 3
         self.board = np.zeros([self.rows, self.columns])
         self.end_game = False
-        # self.player1 = agent1
-        # self.player2 = agent2
+        self.state_hash_matrix = np.array([[10**1, 10**2, 10**3],
+                                           [10**4, 10**5, 10**6],
+                                           [10**7, 10**8, 10**9]])
+        self.action_hash_matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+    def get_current_state(self):
+        return self.board
+
+    def get_available_actions(self):
+        return np.argwhere(self.board == 0)
     
+    def get_state_hash(self, state):
+        state_hash = 0
+        product = np.multiply(state, self.state_hash_matrix)
+        state_hash = np.sum(product)
+        # print("Product: ",product)
+        # print("State Hash: ",state_hash)
+        return state_hash
+
+    def get_action_hash(self, actions):
+        hash_list = []
+        for action in actions:
+            x_val = action[0]
+            y_val = action[1]
+            hash_list.append(self.action_hash_matrix[x_val][y_val])
+        return hash_list
+
     def check_game_end():
         pass
 
     def reset_environment():
         pass
-
-    def get_current_state(self):
-        return self.board
