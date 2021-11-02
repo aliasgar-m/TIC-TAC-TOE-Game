e40f315d643091848284f3e35a0a3373940ee6fd
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-11-03 00:13:35 +0530

    updated main for loop

--- src/main.py
+++ src/main.py
@@ -1,5 +1,49 @@
 import numpy as np
+from tqdm import tqdm
+from environment import Environment
+from agent import Agent
+from trainer import Trainer
 
-a = [0,0,0,1]
-bol = all(v == 0 for v in a)
-print(bol)
+def main():
+    game = Environment()
+    p1 = Agent(env=game, sym=1)
+    p2 = Agent(env=game, sym=2)
+    train = Trainer(env=game)
+    current_player = p1
+    # print("Starting State")
+    # print(game.board)
+
+    for i in tqdm(range(1), desc="Progress: "): #replace 1000 by train.iterations
+        while game.end_game is False:
+            current_player.step()
+            # print("State after action")
+            # print(game.board)
+            # print()
+            game.check_winner()
+
+            if game.end_game:
+                game.reset_environment()
+                p1.reset_agent()
+                p2.reset_agent()
+                return
+
+            if current_player.symbol == 1:
+                current_player = p2
+            else:
+                current_player = p1
+
+def main2():
+    arr = np.array([[1,1,1],[0,2,0],[0,0,2]])
+    ans = np.all(arr == 1, axis=1)
+    print(arr)
+    print(ans)
+    if ans.any():
+        print("TRue")
+
+    # arr = np.array([[1,0,0],[1,2,0],[1,0,2]])
+    # ans = np.all(arr == 1, axis=0)
+    # print(arr)
+    # print(ans)
+
+if __name__ == "__main__":
+    main2()
