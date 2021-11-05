27b17ec80d920d62e986a804e1fd2a4a503b4a65
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-11-05 16:57:43 +0530

    changed number of iterations and added tqdm

--- main.py
+++ main.py
@@ -8,22 +8,22 @@ def main():
     game = Environment()
     player_1 = Agent(env=game, sym=1)
     player_2 = Agent(env=game, sym=2)
-    iterations = 1
+    iterations = 500000
     current_player = player_1
     previous_player = None
-    print("Starting State: ")
-    print(game.board)
-    print()
+    # print("Starting State: ")
+    # print(game.board)
+    # print()
 
-    for i in range(iterations):
+    for i in tqdm(range(iterations)):
         while game.end_game is False:
 
             current_player.step()
-            print("Current Player: ", current_player.symbol)
-            print("Action taken: ", current_player.action_history[-1])
-            print("New State: ")
-            print(game.board)
-            print()
+            # print("Current Player: ", current_player.symbol)
+            # print("Action taken: ", current_player.action_history[-1])
+            # print("New State: ")
+            # print(game.board)
+            # print()
             game.end_game = game.check_winner(sym=current_player.symbol, state=game.board)
             previous_player = current_player
 
@@ -44,7 +44,6 @@ def main():
     print(game.summary)
     print()
     print("Agent 1 Q values: ", player_1.trainer.q_values)
-    print()
     print()
     print("Agent 2 Q values: ", player_2.trainer.q_values)
 
