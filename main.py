d1fbc91414a7f35dbf912fe8c2cae5cf5f1fce66
Aliasgar Merchant <aliasgarm2000@gmail.com>
2021-11-07 18:21:36 +0530

    adding code for human vs comp

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import numpy as np
 from tqdm import tqdm
 from src.environment import Environment
 from src.agent import Agent
+import pickle
 
+BDICT = {}
 
 def main():
     game = Environment()
     player_1 = Agent(env=game, sym=1)
     player_2 = Agent(env=game, sym=2)
-    iterations = 500000
+    iterations = 50
     current_player = player_1
     previous_player = None
     # print("Starting State: ")
@@ -41,11 +43,28 @@ def main():
 
         game.reset_environment()
 
-    print(game.summary)
-    print()
+
     print("Agent 1 Q values: ", player_1.trainer.q_values)
     print()
     print("Agent 2 Q values: ", player_2.trainer.q_values)
+    print(game.summary)
+    print()
+
+    if player_1.wins > player_2.wins:
+        save_q_values(agent=player_1, filename='Q_values.txt')
+    else:
+        save_q_values(agent=player_2, filename='Q_values.txt')
+
+def save_q_values(agent:Agent, filename:str):
+    with open(filename, 'wb') as dict_items_save:
+        pickle.dump(agent.trainer.q_values, dict_items_save)
+
+def load_q_values(filename:str):
+    with open(filename, 'rb') as dict_items_open:
+        BDICT = pickle.load(dict_items_open)
+        return BDICT
 
 if __name__ == "__main__":
     main()
+    file='Q_values.txt'
+    # load_q_values(filename=file)
